9c04e472f88b25cddb517eb4b0eb2bda63937b43
nirmalearn <132821831+nirmalearn@users.noreply.github.com>
2024-09-24 10:35:36 +0530

    Add files via upload

---
+++ BuildVM/CreateHyperVVM.ps1
@@ -0,0 +1,14 @@
+﻿ $VMName = "VMNAME"
+
+ $VM = @{
+     Name = $VMName
+     MemoryStartupBytes = 2147483648
+     Generation = 2
+     NewVHDPath = "C:\Users\abni2\Desktop\Hyper-V\web_server_g2\WinDev2407Eval3.vhdx"
+     NewVHDSizeBytes = 53687091200
+     BootDevice = "VHD"
+     Path = "C:\Users\abni2\Desktop\Hyper-V\$VMName"
+     SwitchName = (Get-VMSwitch).Name[0]
+ }
+
+ New-VM @VM
